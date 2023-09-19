60c030c0ada8a45f4ee942aca925dcac15b635fd
harshilrajguru <144790154+harshilrajguru@users.noreply.github.com>
2023-09-19 13:02:03 +0530

    Add files via upload

---
+++ array1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+	int a[10],i;
+	printf("store the elements in array:");
+	for(i=0;i<=9;i++)
+	  {
+	  	printf("The stored arrays are:");
+	  	scanf("\n %d",&a[i]);
+	  }
+}
